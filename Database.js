5da058bc878e4bb237b3c23d3f32ea24f4a2b6c9
efratKRYT <epinyan@g.jct.ac.il>
2023-07-25 12:53:51 +0300

    database-finish

--- Database.js
+++ Database.js
@@ -1,7 +1,3 @@
-const cors = require('cors');
-const express = require('express')
-const app = express()
-
 const mysql = require('mysql2');
 
 // app.listen(3001, () => {
@@ -10,146 +6,167 @@ const mysql = require('mysql2');
 
 // MySQL database connection configuration
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'ycGB1625',
-    database: 'booksShop',
-    port: '3306'
+  host: 'localhost',
+  user: 'root',
+  password: 'mySql123',
+  database: 'booksShop',
+  port: '3306',
 });
 
-// SQL script to create the database and tables
-const sqlScript = `
-CREATE DATABASE booksShop;
-USE booksShop;
+//SQL script to create the database and tables
 
--- Place the provided SQL script here
--- ...
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    Username VARCHAR(255),
-    Password VARCHAR(255),
-    Email VARCHAR(255),
-    First_name VARCHAR(255),
-    Last_Name VARCHAR(255),
-    Address VARCHAR(255),
-    Phone_Number VARCHAR(10),
-    birthday_date DATE,
-    Exercising_a_birthday_discount BOOLEAN
-);
+// const sqlScript = `
+// CREATE DATABASE booksShop;
 
-CREATE TABLE Employees (
-    manager_id INT PRIMARY KEY AUTO_INCREMENT,
-    Username VARCHAR(255),
-    Password VARCHAR(255),
-    Email VARCHAR(255),
-    First_name VARCHAR(255),
-    Last_Name VARCHAR(255),
-    Address_VARCHAR(255),
-    Phone_Number VARCHAR(255)
-);
+//   USE booksShop;
 
-CREATE TABLE Products (
-    Product_ID INT PRIMARY KEY AUTO_INCREMENT,
-    Title VARCHAR(255),
-    Author VARCHAR(255),
-    Description TEXT,
-    Price DECIMAL(10, 2),
-    Category VARCHAR(255),
-    Amount INT
-);
+//   CREATE TABLE Customers (
+//     customer_id INT PRIMARY KEY AUTO_INCREMENT,
+//     Username VARCHAR(255),
+//     Password VARCHAR(255),
+//     Email VARCHAR(255),
+//     First_name VARCHAR(255),
+//     Last_Name VARCHAR(255),
+//     Address VARCHAR(255),
+//     Phone_Number VARCHAR(10),
+//     birthday_date DATE,
+//     Exercising_a_birthday_discount BOOLEAN
+// );
 
-CREATE TABLE Orders (
-    Order_ID INT PRIMARY KEY AUTO_INCREMENT,
-    Name_of_the_owner_of_the_order VARCHAR(255),
-    shipping_address VARCHAR(255),
-    Delivery_city VARCHAR(255),
-    Mail VARCHAR(255),
-    Telephone VARCHAR(255),
-    Credit_details_for_payment VARCHAR(255),
-    customer_id INT,
-    The_final_order_price DECIMAL(10, 2),
-    purchase_date TIMESTAMP,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
 
-CREATE TABLE Purchase_of_items (
-    item_id INT PRIMARY KEY AUTO_INCREMENT,
-    purchase_id INT,
-    product_id INT,
-    Amount INT,
-    Price DECIMAL(10, 2),
-    FOREIGN KEY (purchase_id) REFERENCES Orders(Order_ID),
-    FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
-);
+//   CREATE TABLE Employees (
+//       manager_id INT PRIMARY KEY AUTO_INCREMENT,
+//       Username VARCHAR(255),
+//       Password VARCHAR(255),
+//       Email VARCHAR(255),
+//       First_name VARCHAR(255),
+//       Last_Name VARCHAR(255),
+//       Address_VARCHAR(255),
+//       Phone_Number VARCHAR(255)
+//   );
 
-CREATE TABLE Shopping_cart (
-    cart_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    product_id INT,
-    Amount INT,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-    FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
-);
+//   CREATE TABLE Products (
+//       Product_ID INT PRIMARY KEY AUTO_INCREMENT,
+//       Title VARCHAR(255),
+//       Author VARCHAR(255),
+//       Description TEXT,
+//       Price DECIMAL(10, 2),
+//       Category VARCHAR(255),
+//       Amount INT
+//   );
 
-CREATE TABLE Account_page (
-    account_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    order_id INT,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-    FOREIGN KEY (order_id) REFERENCES Orders(Order_ID)
-);
+//   CREATE TABLE Orders (
+//       Order_ID INT PRIMARY KEY AUTO_INCREMENT,
+//       Name_of_the_owner_of_the_order VARCHAR(255),
+//       shipping_address VARCHAR(255),
+//       Delivery_city VARCHAR(255),
+//       Mail VARCHAR(255),
+//       Telephone VARCHAR(255),
+//       Credit_details_for_payment VARCHAR(255),
+//       customer_id INT,
+//       The_final_order_price DECIMAL(10, 2),
+//       purchase_date TIMESTAMP,
+//       FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+//   );
 
-CREATE TABLE Usernames_and_passwords (
-    Username VARCHAR(255),
-    Password VARCHAR(255),
-    Allowing_Access BOOLEAN
-);
+//   CREATE TABLE Purchase_of_items (
+//       item_id INT PRIMARY KEY AUTO_INCREMENT,
+//       purchase_id INT,
+//       product_id INT,
+//       Amount INT,
+//       Price DECIMAL(10, 2),
+//       FOREIGN KEY (purchase_id) REFERENCES Orders(Order_ID),
+//       FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
+//   );
 
-CREATE TABLE Tracking_the_price_of_books_in_the_sale (
-    price_id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id INT,
-    Price_after_sale DECIMAL(10, 2),
-    Time_stamp TIMESTAMP,
-    promotion_id INT,
-    FOREIGN KEY (promotion_id) REFERENCES Promotions(promotion_id),
-    FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
-);
+//   CREATE TABLE Shopping_cart (
+//       cart_id INT PRIMARY KEY AUTO_INCREMENT,
+//       customer_id INT,
+//       product_id INT,
+//       Amount INT,
+//       FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+//       FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
+//   );
 
-CREATE TABLE Inventory_tracking (
-    inventory_id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id INT,
-    Amount INT,
-    Time_stamp TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
-);
+//   CREATE TABLE Account_page (
+//       account_id INT PRIMARY KEY AUTO_INCREMENT,
+//       customer_id INT,
+//       order_id INT,
+//       FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+//       FOREIGN KEY (order_id) REFERENCES Orders(Order_ID)
+//   );
 
-CREATE TABLE Promotions (
-    promotion_id INT PRIMARY KEY AUTO_INCREMENT,
-    Title VARCHAR(255),
-    Type VARCHAR(255),
-    Description TEXT,
-    Start_Date DATE,
-    end_date DATE
-);
+//   CREATE TABLE Usernames_and_passwords (
+//       Username VARCHAR(255),
+//       Password VARCHAR(255),
+//       Allowing_Access BOOLEAN
+//   );
 
-CREATE TABLE Notifications (
-    alert_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    message TEXT,
-    Time_stamp TIMESTAMP,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
+//   CREATE TABLE Tracking_the_price_of_books_in_the_sale (
+//       price_id INT PRIMARY KEY AUTO_INCREMENT,
+//       product_id INT,
+//       Price_after_sale DECIMAL(10, 2),
+//       Time_stamp TIMESTAMP,
+//       promotion_id INT,
+//       FOREIGN KEY (promotion_id) REFERENCES Promotions(promotion_id),
+//       FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
+//   );
 
-CREATE TABLE Order_tracking (
-    tracking_id INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    Status VARCHAR(255),
-    Time_stamp TIMESTAMP,
-    FOREIGN KEY (order_id) REFERENCES Orders(Order_ID)
-);
-`;
+//   CREATE TABLE Inventory_tracking (
+//       inventory_id INT PRIMARY KEY AUTO_INCREMENT,
+//       product_id INT,
+//       Amount INT,
+//       Time_stamp TIMESTAMP,
+//       FOREIGN KEY (product_id) REFERENCES Products(Product_ID)
+//   );
 
-// Connect to MySQL and execute the SQL script
+//   CREATE TABLE Promotions (
+//       promotion_id INT PRIMARY KEY AUTO_INCREMENT,
+//       Title VARCHAR(255),
+//       Type VARCHAR(255),
+//       Description TEXT,
+//       Start_Date DATE,
+//       end_date DATE
+//   );
+
+//   CREATE TABLE Notifications (
+//       alert_id INT PRIMARY KEY AUTO_INCREMENT,
+//       customer_id INT,
+//       message TEXT,
+//       Time_stamp TIMESTAMP,
+//       FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+//   );
+
+//   CREATE TABLE Order_tracking (
+//       tracking_id INT PRIMARY KEY AUTO_INCREMENT,
+//       order_id INT,
+//       Status VARCHAR(255),
+//       Time_stamp TIMESTAMP,
+//       FOREIGN KEY (order_id) REFERENCES Orders(Order_ID)
+//   );
+//   `;
+
+
+
+// const insertData = {
+//   Username: 'example_user',
+//   Password: 'example_password',
+//   Allowing_Access: true
+// };
+
+// const insertQuery = 'INSERT INTO Usernames_and_passwords (Username, Password, Allowing_Access) VALUES (?, ?, ?)';
+
+// connection.query(insertQuery, [insertData.Username, insertData.Password, insertData.Allowing_Access], (err, results) => {
+//   if (err) {
+//     console.error('Error inserting data:', err);
+//   } else {
+//     console.log('Data inserted successfully.');
+//     console.log('Inserted row ID:', results.insertId);
+//   }
+//   connection.end();
+// });
+
+//Connect to MySQL and execute the SQL script
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
@@ -166,6 +183,17 @@ connection.connect((err) => {
       console.log('SQL script executed successfully.');
       console.log('Results:', results);
     }
+
+    // Execute each statement separately
+    sqlStatements.forEach((sqlStatement) => {
+      connection.query(sqlStatement, (error, results) => {
+        if (error) {
+          console.error('Error executing SQL statement:', error);
+        } else {
+          console.log('SQL statement executed successfully.');
+        }
+      });
+    });
 
     // Close the connection
     connection.end((err) => {
